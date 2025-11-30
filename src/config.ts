a4eeb8279128c107ce2fb2f53d6d43d0f44b40a1
JaxylViernes <144340288+JaxylViernes@users.noreply.github.com>
2025-11-30 16:43:42 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // export const backendPrefix = 'https://viralmotion-backend-f1m2.onrender.com';
-// export const backendPrefix  = "https://viralmotion-backend-vyis.onrender.com";
-export const backendPrefix = 'http://localhost:3000';
+export const backendPrefix  = "https://viralmotion-backend-vyis.onrender.com";
+// export const backendPrefix = 'http://localhost:3000';
